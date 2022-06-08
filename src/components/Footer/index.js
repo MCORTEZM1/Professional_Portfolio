22346ec7c4681b74830a069ef9fdafd2045c2be4
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-06-07 22:17:03 -0700

    import style css to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './style.css';
 
 
 function Footer() { 
