7d6c566205eb3398d0cf65e7bb58cab6c39e6741
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-06-10 14:07:03 -0700

    refactor projectCard componen

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -14,34 +14,34 @@ function ProjectCard() {
             >
                 {projects.map((project, i) => (
                         
+                        <div 
+                            className="card col-12 project-card"
+                            key={i}
+                            id={project.name}
+                            onClick={() => window.open(`${project.deployed}`, '_blank')}
+                        >
                             <div 
-                                className="card col-12 project-card"
-                                key={i}
-                                id={project.name}
-                                onClick={() => window.open(`${project.deployed}`, '_blank')}
-                            >
-                                <div 
-                                    className="card-header text-center"
-                                    id='card-header'
-                                >{project.name}
-                                </div>
-                                <img 
-                                    src={project.image}  
-                                    alt={project.name} 
-                                    id='card-image'
-                                />
-                                <div 
-                                    className={`card-body`} 
-                                    id='card-description'
-                                    >{project.description}
-                                   <h6 className='mt-2'> Tools:</h6>
-                                    <ul className="mt-2 ">
-                                        {project.tools.map((tool, ) => (
-                                            <li>{tool}</li>
-                                        ))}
-                                    </ul>
-                                </div>
+                                className="card-header text-center"
+                                id='card-header'
+                            >{project.name}
                             </div>
+                            <img 
+                                src={project.image}  
+                                alt={project.name} 
+                                id='card-image'
+                            />
+                            <div 
+                                className={`card-body`} 
+                                id='card-description'
+                                >{project.description}
+                                <h6 className='mt-2'> Tools:</h6>
+                                <ul className="mt-2 ">
+                                    {project.tools.map((tool, ) => (
+                                        <li>{tool}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
                 ))}
            </div>  
        </div>
